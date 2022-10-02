6a96af9d50aaf3079c2dae24c1ed7d6fcca35b70
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-10-02 17:35:39 +0300

    Методы.Урок с Ильёй Сазоновым.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Методы.Урок с Ильёй Сазоновым.
+        System.out.println("Методы.Урок с Ильёй Сазоновым.");
+
+
+
+    }
+}
